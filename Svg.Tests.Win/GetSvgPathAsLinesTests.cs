29055b80aee16c4396ac2846e509ad92f04536cc
Zeljko Predjeskovic <Zeljko.Predjeskovic@opti-q.com>
2022-09-19 13:42:38 +0200

    Test SvgPathSegmentListExtension

---
+++ Svg.Tests.Win/GetSvgPathAsLinesTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using FluentAssertions;
+using FluentAssertions.Common;
+using NUnit.Framework;
+using Svg.Interfaces;
+using Svg.Pathing;
+
+namespace Svg.Tests.Win
+{
+    [TestFixture]
+    public class GetSvgPathAsLinesTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            SvgPlatform.Init();
+        }
+
+        [Test]
+        public void GivenSvgPathSegmentList_ReturnsAllVisibleLines()
+        {
+            //Arrange
+            var svgPathSegmentList = new SvgPathSegmentList
+            {
+                new SvgMoveToSegment(PointF.Create(50, 250)),
+                new SvgLineSegment(PointF.Create(50, 250), PointF.Create(50, 50)),
+                new SvgLineSegment(PointF.Create(50, 50), PointF.Create(250, 50)),
+                new SvgClosePathSegment(),
+                new SvgMoveToSegment(PointF.Create(8, 650))
+            };
+            
+            var expected = new []
+            {
+                (PointF.Create(50,250), PointF.Create(50,50)),
+                (PointF.Create(50,50), PointF.Create(250,50)),
+                (PointF.Create(250,50), PointF.Create(50,250)),
+            }.ToList();
+
+            //Act
+            var actual = svgPathSegmentList.GetLines();
+
+            //Assert
+            actual.Should().Equal(expected);
+
+        }
+    }
+}
