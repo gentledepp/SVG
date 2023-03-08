f24a6ace4f3355c94ab3c8a4c3c390d5699169df
amarek <alexander.marek@outlook.com>
2023-03-08 14:24:18 +0100

    Svg.Editor changes zoom focus correctly on UWP

--- Svg.Editor.Core/Events/ScaleEvent.cs
+++ Svg.Editor.Core/Events/ScaleEvent.cs
@@ -26,6 +26,12 @@ namespace Svg.Editor.Events
             FocusY = focusY;
         }
 
+        /// <summary>
+        /// On Windows, we want to be able to change the zoom focus on every scroll wheel event as we are using the mouse
+        /// On iOS and Android, we use touch and in order to not make the zoom too "jumpy", we fix the focus point on zoom
+        /// </summary>
+        public bool ChangeFocus { get; set; }
+
         public override string DebuggerDisplay => $"Scale ({Enum.GetName(typeof(ScaleStatus), Status)}) {ScaleFactor} at x:{FocusX} y:{FocusY}";
 
         public override string ToString()
