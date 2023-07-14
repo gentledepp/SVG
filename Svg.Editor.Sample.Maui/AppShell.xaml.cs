5d26151376ae21ec4109c60c08be69b72f8b7700
Zeljko Predjeskovic <Zeljko.Predjeskovic@opti-q.com>
2023-07-14 11:04:19 +0200

    save commit

---
+++ Svg.Editor.Sample.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Svg.Editor.Sample.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Svg.Editor.Sample.Maui/MauiProgram.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+using SkiaSharp.Views.Forms;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+using Svg.Editor.Forms;
+
+namespace Svg.Editor.Sample.Maui
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            SvgEditorForms.Init();
+            builder
+                .UseMauiApp<Samples.Forms.App>()
+                .UseMauiCompatibility()
+                .UseSkiaSharp()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .ConfigureMauiHandlers(handlers =>
+                {
+#if ANDROID
+                    handlers.AddHandler(typeof(SvgCanvasEditorView), typeof(DroidCanvasViewHandlerBase));
+#elif IOS
+                    handlers.AddHandler(typeof(SvgCanvasEditorView), typeof(TouchCanvasViewHandlerBase));
+#else
+                    handlers.AddHandler(typeof(SvgCanvasEditorView), typeof(UwpCanvasViewHandlerBase));
+#endif
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
