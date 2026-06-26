fd4a2d340c4c555b73f698bb1c088fad4bde93be
Zeljko Predjeskovic <Zeljko.Predjeskovic@opti-q.com>
2026-06-26 10:16:30 +0200

    #13430 - Svg editor choosing Line endings does not change anything

--- Svg.Editor.Avalonia.Forms/Services/MarkerOptionsInputService.cs
+++ Svg.Editor.Avalonia.Forms/Services/MarkerOptionsInputService.cs
@@ -1,4 +1,5 @@
 ﻿using Svg.Editor.Avalon.Forms.Dialog;
+using Svg.Editor.Interfaces;
 using Svg.Editor.Tools;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,22 +9,27 @@ namespace Svg.Editor.Avalon.Forms.Services
 {
     public class MarkerOptionsInputService : IMarkerOptionsInputService
     {
+        private ILocalizationService _localizationService;
+
+        public MarkerOptionsInputService()
+        {
+            _localizationService = SvgEngine.Resolve<ILocalizationService>();
+        }
+
         public async Task<int[]> GetUserInput(string title, IEnumerable<string> markerStartOptions, int markerStartSelected, IEnumerable<string> markerEndOptions,
             int markerEndSelected)
         {
 
-
             var mso = markerStartOptions.ToList();
-            var start = await UserInteractionServiceExt.UserInteractionInst.ActionSheetAsync(title, mso.ToArray());
+            var start = await UserInteractionServiceExt.UserInteractionInst.ActionSheetAsync(_localizationService.GetString("Marker.Options.Start"), mso.ToArray(), cancelButton: "Cancel", cancellable: true);
 
-            await UserInteractionServiceExt.UserInteractionInst.ActionSheetAsync(title, mso.ToArray());
 
             var startIndex = mso.IndexOf(start);
             if (start == null || startIndex < 0)
                 return new[] { markerStartSelected, markerEndSelected };
 
             var meo = markerEndOptions.ToList();
-            var end = await UserInteractionServiceExt.UserInteractionInst.ActionSheetAsync(title, mso.ToArray());
+            var end = await UserInteractionServiceExt.UserInteractionInst.ActionSheetAsync(_localizationService.GetString("Marker.Options.End"), meo.ToArray(), cancelButton: "Cancel", cancellable: true);
 
             var endIndex = meo.IndexOf(end);
             if (end == null || endIndex < 0)
